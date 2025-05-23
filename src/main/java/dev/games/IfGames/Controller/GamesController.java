187b4128a0b30946852294b172e20ab070a19c69
Douglas Machado <douglasvilla23@gmail.com>
2025-05-23 19:26:47 -0300

    Feat: Cria a pasta Controller e a classe Games Controller no programa

---
+++ src/main/java/dev/games/IfGames/Controller/GamesController.java
@@ -0,0 +1,9 @@
+package dev.games.IfGames.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class GamesController {
+}
