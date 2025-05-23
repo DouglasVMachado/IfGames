a2671a73f5e1c00ce2eb90f6c0ee41dde0afc444
Douglas Machado <douglasvilla23@gmail.com>
2025-05-23 00:27:38 -0300

    first commit

---
+++ src/main/java/dev/games/IfGames/IfGamesApplication.java
@@ -0,0 +1,13 @@
+package dev.games.IfGames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfGamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/games/IfGames/IfGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.games.IfGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
