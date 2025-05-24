3179575b0dd04350131780dbf98ad420482d4bac
Douglas Machado <douglasvilla23@gmail.com>
2025-05-24 01:18:09 -0300

    Feat: Cria o pacote Entity e a classe GamesEntity

---
+++ src/main/java/dev/games/IfGames/Entity/GamesEntity.java
@@ -0,0 +1,58 @@
+package dev.games.IfGames.Entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Games")
+public class GamesEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private int lançamento;
+    private String name;
+    private Long preco;
+
+    public GamesEntity() {
+    }
+
+    public GamesEntity(Long id, String name, int lançamento, Long preco) {
+        this.id = id;
+        this.name = name;
+        this.lançamento = lançamento;
+        this.preco = preco;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLançamento() {
+        return lançamento;
+    }
+
+    public void setLançamento(int lançamento) {
+        this.lançamento = lançamento;
+    }
+
+    public Long getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Long preco) {
+        this.preco = preco;
+    }
+}
