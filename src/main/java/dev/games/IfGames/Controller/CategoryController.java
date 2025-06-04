027e6b996449c719f58a03a5533ab909851aafb5
Douglas Machado <douglasvilla23@gmail.com>
2025-06-04 00:05:51 -0300

    Feat: cria as endpoint no controller

--- src/main/java/dev/games/IfGames/Controller/CategoryController.java
+++ src/main/java/dev/games/IfGames/Controller/CategoryController.java
@@ -1,4 +1,35 @@
 package dev.games.IfGames.Controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/category")
 public class CategoryController {
+
+    @PostMapping("/create")
+    public String createCategory(){
+        return "criar categoria";
+    }
+
+    @GetMapping("/search")
+    public String searchGame(){
+        return "Procurar algum jogo";
+    }
+
+    @GetMapping("/list")
+    public String listAllGames(){
+        return "Listar todos os jogos";
+    }
+
+    @PutMapping("/update")
+    public String updateGame(){
+        return "Atualizar um jogo";
+    }
+
+    @DeleteMapping("/delete")
+    public String deleteGame(){
+        return "Deletar o jogo";
+    }
+
+
 }
--- src/main/java/dev/games/IfGames/Controller/GamesController.java
+++ src/main/java/dev/games/IfGames/Controller/GamesController.java
@@ -1,9 +1,40 @@
 package dev.games.IfGames.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import dev.games.IfGames.Entity.GamesModel;
+import dev.games.IfGames.Service.GamesService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping
+@RequestMapping("/games")
 public class GamesController {
+
+    private GamesService gamesService;
+
+    @GetMapping("/search")
+    public String searchById(){
+        return "buscando jogos por id";
+    }
+
+    @GetMapping("/list")
+    public List<GamesModel> listAllGames(){
+        return gamesService.listAllGames();
+    }
+
+    @PostMapping("/create")
+    public String insertGame(){
+        return "Inserir o jogo";
+    }
+
+    @PutMapping("/update")
+    public String updateGame(){
+        return "atualizar algum jogo";
+    }
+
+    @DeleteMapping("/Delete")
+    public String DeleteGame(){
+        return "Deletar um jogo";
+    }
+
 }
