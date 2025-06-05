a45653045ad807f88ceaecda60f7b130642daff4
Douglas Machado <douglasvilla23@gmail.com>
2025-06-04 23:55:11 -0300

    eat: Cria CategoryService e os get para listar

---
+++ src/main/java/dev/games/IfGames/Service/CategoryService.java
@@ -0,0 +1,28 @@
+package dev.games.IfGames.Service;
+
+import dev.games.IfGames.Entity.CategoryModel;
+import dev.games.IfGames.Repository.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    public List<CategoryModel> listAllCategory(){
+        return categoryRepository.findAll();
+    }
+
+    public CategoryModel searchCategory(Long id){
+        Optional<CategoryModel> categoryModel = categoryRepository.findById(id);
+        return categoryModel.orElse(null);
+    }
+
+}
