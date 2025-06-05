7f217bb7aa4a65f7f2ec4f9744a9ef90c01f46b6
Douglas Machado <douglasvilla23@gmail.com>
2025-06-04 23:52:41 -0300

    Feat: Cria o CategoryService e as os @GetMapping

--- src/main/java/dev/games/IfGames/Service/GamesService.java
+++ src/main/java/dev/games/IfGames/Service/GamesService.java
@@ -5,6 +5,7 @@ import dev.games.IfGames.Repository.GamesRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GamesService {
@@ -17,6 +18,11 @@ public class GamesService {
 
     public List<GamesModel> listAllGames(){
         return gamesRepository.findAll();
+    }
+
+    public GamesModel searchById(Long id){
+        Optional<GamesModel> gameById = gamesRepository.findById(id);
+        return gameById.orElse(null);
     }
 
 }
