b3660cb49125aa7e86e7ac6521778f8d2208a352
Douglas Machado <douglasvilla23@gmail.com>
2025-06-06 23:47:03 -0300

    Feat: Cria o Mapper e DTO e faz a junçao do DTO com o Model nas classes

--- src/main/java/dev/games/IfGames/DTO/CategoryDTO.java
+++ src/main/java/dev/games/IfGames/DTO/CategoryDTO.java
@@ -1,4 +1,20 @@
 package dev.games.IfGames.DTO;
 
+
+import dev.games.IfGames.Entity.GamesModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class CategoryDTO {
+
+    private Long id;
+    private String category;
+    private List<GamesModel> games;
+
 }
--- src/main/java/dev/games/IfGames/DTO/GamesDTO.java
+++ src/main/java/dev/games/IfGames/DTO/GamesDTO.java
@@ -1,4 +1,22 @@
 package dev.games.IfGames.DTO;
 
+import dev.games.IfGames.Entity.CategoryModel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class GamesDTO {
+
+
+    private Long id;
+    private int lançamento;
+    private String name;
+    private Long preco;
+    private String imgUrl;
+    private String dificuldade;
+    private CategoryModel category;
+
 }
